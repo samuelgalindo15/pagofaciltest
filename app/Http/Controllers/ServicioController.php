b2a2887a6a971b933436ea138aca08348629fb84
Samuel Galindo <original_hotmail.com>
2017-03-25 14:38:38 -0600

    Creacion Controlador y rutas

---
+++ app/Http/Controllers/ServicioController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServicioController extends Controller
+{
+    /**
+     * Muestra a todos los empleados
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Muestra el formulario para crear al nuevo empleado
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Guarda al nuevo empleado
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Muestra los datos de un empleado en específico
+     *
+     * @param  string  $dato
+     * @return \Illuminate\Http\Response
+     */
+    public function show($dato)
+    {
+        //
+    }
+
+    /**
+     * Muestra el formulario para editar al empleado
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Actualiza a un empleado en específico
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Elimina a un empleado en específico
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
